b5925088e1cb3e2a4092815ce8aad7325cd7c1e6
Nikita8Sannikov <sannikovnikita@gmail.com>
2024-11-21 16:07:11 +0600

    add express server with User model & auth route

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import config from "config";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.routes.js";
+import usersRoutes from "./routes/users.routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+
+const PORT = config.get("port") || 5000;
+
+async function start() {
+	try {
+		await mongoose.connect(config.get("mongoUri"), {});
+		app.listen(PORT, () =>
+			console.log(`App has been started on port ${PORT}`)
+		);
+	} catch (e) {
+		if (e instanceof Error) {
+			console.log("Server Error", e.message);
+		} else {
+			console.log("Server Error", e);
+		}
+		process.exit(1);
+	}
+}
+
+start();

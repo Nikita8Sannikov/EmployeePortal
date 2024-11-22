0d2cce440abac1fac90f804a065d77042d314219
Nikita <sannikovnikita1992@gmail.com>
2024-11-22 17:02:33 +0600

    comment normalize & delete some  comments

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -59,12 +59,11 @@ router.post(
 router.post(
 	"/login",
 	[
-		check("email", "Correct email required").normalizeEmail().isEmail(),
+		// check("email", "Correct email required").normalizeEmail().isEmail(),
+		check("email", "Correct email required").isEmail(),
 		check("password", "Password required").exists(),
 	],
 	async (req, res) => {
-		// console.log(req.body);
-
 		try {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) {
@@ -74,12 +73,12 @@ router.post(
 				});
 				return;
 			}
-			const { email, password, name } = req.body;
+			const { email, password } = req.body;
 
 			const user = await User.findOne({ email });
 
 			if (!user) {
-				res.status(400).json({ message: "User not found" });
+				res.status(404).json({ message: "User not found" });
 				return;
 			}
 
@@ -93,14 +92,16 @@ router.post(
 			const token = jwt.sign({ userId: user.id }, jwtSecret, {
 				expiresIn: "1h",
 			});
+
 			res.status(200).json({
 				token,
-				userId: user.id,
+				_id: user.id,
 				email: user.email,
 				name: user.name,
+				first_name: user.first_name,
+				last_name: user.last_name,
+				avatar: user.avatar,
 			});
-			// console.log("user", user);
-			// console.log("userId", user.id);
 		} catch (e) {
 			res.status(500).json({ message: "Smth wrong, try again" });
 		}
@@ -117,10 +118,9 @@ router.get("/me", async (req, res) => {
 
 		const decoded = jwt.verify(token, jwtSecret);
 		const userId = decoded.userId;
-		// console.log(req);
+
 		const user = await User.findById(userId);
-		// console.log(user);
-		// 6737165ce0b86163cb6959a2
+
 		if (!user) {
 			return res.status(404).json({ message: "User not found" });
 		}

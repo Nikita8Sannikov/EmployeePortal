418eaeb5123ba717595edb1cdad12600929f2a93
Nikita8Sannikov <sannikovnikita1992@gmail.com>
2024-11-25 12:21:24 +0600

    add new field to User schema

--- models/User.js
+++ models/User.js
@@ -4,9 +4,9 @@ const schema = new Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	name: { type: String, required: true },
-	// first_name: { type: String, required: true },
-	// last_name: { type: String, required: true },
-	// avatar: { type: String, default: "https://i.ibb.co/4pDNDk1/avatar.png" },
+	first_name: { type: String, required: true },
+	last_name: { type: String, required: true },
+	avatar: { type: String, default: "https://i.ibb.co/4pDNDk1/avatar.png" },
 	// isAdmin: {type: Boolean, default: false}
 });
 

d1114b05d6a5f6c88a7142377b91fadb216c3b05
Nikita8Sannikov <sannikovnikita1992@gmail.com>
2024-12-12 16:26:56 +0600

    feat: add AppService to initialize MainCore and add it to context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import App from "./components/app";
+// import App from "./components/app";
 import "./global.css";
+import AppService from "./service/AppService";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</StrictMode>
+	// <StrictMode>
+	<Provider store={store}>
+		<AppService />
+	</Provider>
+	// </StrictMode>
 );

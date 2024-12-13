ff27fc5db2acbdd7b1c4633b958b3c822a95a4c7
Nikita8Sannikov <sannikovnikita1992@gmail.com>
2024-12-13 13:40:15 +0600

    feat: add setUser method to UserController class

--- client/src/core/UsersController.ts
+++ client/src/core/UsersController.ts
@@ -20,8 +20,8 @@ export default class UsersController {
     }
 
     get users() {
-        console.log('this._users.values()', this._users.values());
-        console.log('Array.from(this._users.values()', Array.from(this._users.values()));
+        // console.log('this._users.values()', this._users.values());
+        // console.log('Array.from(this._users.values()', Array.from(this._users.values()));
 
         // console.log('_authUser', this._authUser);
 
@@ -30,19 +30,28 @@ export default class UsersController {
 
     setAuthUser(user: IUser) {
         this._authUserId = user._id;
-        console.log('SET AUTH USER -> this._authUserId', this._authUserId);
+        // console.log('SET AUTH USER -> this._authUserId', this._authUserId);
 
         this._users.set(user._id, (user.isAdmin ? new Admin(user) : new User(user)))
-        console.log(this._users);
+        // console.log(this._users);
+
+    }
+
+    setUser(user: IUser) {
+        // this._authUserId = user._id;
+        // console.log('SET AUTH USER -> this._authUserId', this._authUserId);
+
+        this._users.set(user._id, (user.isAdmin ? new Admin(user) : new User(user)))
+        // console.log(this._users);
 
     }
 
 
     //возвращает мой объект пользователя(любого)
     getUser(userId = this._authUserId) {
-        console.log('getUser -> this._authUserId', this._authUserId);
-        console.log('getUser -> userId', userId);
-        console.log('ВСЕ ЮЗЕРЫ', this._users);
+        // console.log('getUser -> this._authUserId', this._authUserId);
+        // console.log('getUser -> userId', userId);
+        // console.log('ВСЕ ЮЗЕРЫ', this._users);
 
         if (this._users.has(userId)) {
             return this._users.get(userId)!
@@ -59,21 +68,21 @@ export default class UsersController {
     //     return this._authUserId
     // }
 
-    async fetchUserById(id: string) {
-        try {
-            const response = await fetch(`/api/users/${id}`);
-            if (!response.ok) {
-                throw new Error("Failed to fetch user");
-            }
-            const user = await response.json();
-            return user
-            // if (user) {
-            //     this._authUser = user;
-            // }
-        } catch (e) {
-            console.error('Ошибка при загрузке пользователя:', e);
-        }
-    }
+    // async fetchUserById(id: string) {
+    //     try {
+    //         const response = await fetch(`/api/users/${id}`);
+    //         if (!response.ok) {
+    //             throw new Error("Failed to fetch user");
+    //         }
+    //         const user = await response.json();
+    //         return user
+    //         // if (user) {
+    //         //     this._authUser = user;
+    //         // }
+    //     } catch (e) {
+    //         console.error('Ошибка при загрузке пользователя:', e);
+    //     }
+    // }
 
     async fetchUsers(page: number) {
         try {
@@ -86,7 +95,7 @@ export default class UsersController {
                 body: JSON.stringify({ page, authUserId: this._authUserId }),
             })
             const result = await response.json()
-            console.log('РЕЗУЛЬТАТ ПОЛУЧЕНИЯ ПОЛЬЗОВАТЕЛЕЙ', result);
+            // console.log('РЕЗУЛЬТАТ ПОЛУЧЕНИЯ ПОЛЬЗОВАТЕЛЕЙ', result);
             // if (page === 1) {
             //     this._users.clear()
             // }
@@ -97,7 +106,7 @@ export default class UsersController {
                         this._users.delete(user._id)
                     }
                     this._users.set(user._id, (user.isAdmin ? new Admin(user) : new User(user)))
-                    console.log('ДОБАВИЛИ ЮЗЕРА', this._users);
+                    // console.log('ДОБАВИЛИ ЮЗЕРА', this._users);
                 })
 
 
